377126b2ea749a948643b6023d7eb9ca641492ae
Petr Bukov <pbu.vsl@icloud.com>
2025-02-24 12:51:07 +0100

    feat: add adaptive styles for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,30 @@
+import { useState } from "react";
+
 import logo from "../../assets/logo.svg";
+import menu from "../../assets/menu.svg";
+import close from "../../assets/close.svg";
 import cart from "../../assets/cart.svg";
 import "./Header.scss";
 
 export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  console.log("isMenuOpen", isMenuOpen);
+
   return (
     <header className="header">
       <div className="header__inner">
         <a href="/" className="header__logo">
           <img src={logo} alt="logo" />
         </a>
+        <button
+          className={`header__menu-button header__menu-button--${
+            isMenuOpen ? "opened" : "closed"
+          }`}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <img src={isMenuOpen ? close : menu} alt="logo" />
+        </button>
         <nav className="header__menu">
           <ul className="header__menu-list">
             <li className="header__menu-list-item">
