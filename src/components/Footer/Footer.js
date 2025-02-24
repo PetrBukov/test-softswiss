e84e8746ea774d844b91a889a82fea301919094e
Petr Bukov <pbu.vsl@icloud.com>
2025-02-24 17:43:56 +0100

    feat: add animation for rocket icon to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
+
 import rocketImg from "../../assets/rocket.png";
 import "./Footer.scss";
 
 export const Footer = () => {
+  const [isFlying, setIsFlying] = useState(false);
+
+  useEffect(() => {
+    if (!isFlying) return;
+
+    const timerId = setTimeout(() => {
+      setIsFlying(false);
+    }, 1000);
+
+    return () => clearTimeout(timerId);
+  }, [isFlying]);
+
   return (
     <footer className="footer">
-      <button className="footer__logo">
+      <button
+        disabled={isFlying}
+        className={`footer__logo ${
+          isFlying ? "footer__logo--flying" : "footer__logo--pending"
+        }`}
+        onClick={() => setIsFlying((prev) => !prev)}
+      >
         <img src={rocketImg} alt="cozy rocket icon" />
       </button>
       <div className="footer__text">Exciting space adventure!</div>
